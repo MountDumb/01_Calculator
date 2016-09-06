8464d07a43e963b953ddfd291f47772d29d0919a
UrbanSkurk <UrbanSkurk@VARMEVRK>
2016-09-06 08:46:36 +0200

    Add project files.

---
+++ 01_Calculator/Calculator.cs
@@ -0,0 +1,26 @@
+﻿namespace _01_Calculator
+{
+    internal static class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Sum(int[] a)
+        {
+            int itt = 0;
+
+            foreach (var s in a)
+            {
+                itt += s;
+            }
+            return itt;
+        }
+    }
+}
