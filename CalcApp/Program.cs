44bac76be4dedb590148c72557a31e8b85c2f080
UrbanSkurk <torb3755@edu.eal.dk>
2016-09-08 10:06:38 +0200

    endnu en upload.

--- CalcApp/Program.cs
+++ CalcApp/Program.cs
@@ -11,48 +11,55 @@ namespace CalcApp
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("1: addition 2: Subtraction");
-            //int route = int.Parse(Console.ReadLine());
-            int route = Add();
-            switch (route)
+            bool running = true;
+            while (running == true)
             {
-                case 1:
+                //Console.WriteLine("1: addition 2: Subtraction");
+                //int route = int.Parse(Console.ReadLine());
+                int route = Add();
+                switch (route)
+                {
+                    case 1:
 
 
-                    int a = new int();
-                    int b = new int();
+                        int a = new int();
+                        int b = new int();
 
-                    Console.WriteLine("Tal 1:");
-                    try
-                    {
-                        a = int.Parse(Console.ReadLine());
-                    }
-                    catch
-                    {
-                        Console.WriteLine("et tal, tak");
-                        goto case 1;
-                    }
+                        Console.WriteLine("Tal 1:");
+                        try
+                        {
+                            a = int.Parse(Console.ReadLine());
+                        }
+                        catch
+                        {
+                            Console.WriteLine("et tal, tak");
+                            goto case 1;
+                        }
 
-                    Console.WriteLine("Tal 2:");
-                    try
-                    {
-                        b = int.Parse(Console.ReadLine());
-                    }
-                    catch
-                    {
-                        Console.WriteLine("et tal, tak");
-                        goto case 1;
-                    }
+                        Console.WriteLine("Tal 2:");
+                        try
+                        {
+                            b = int.Parse(Console.ReadLine());
+                        }
+                        catch
+                        {
+                            Console.WriteLine("et tal, tak");
+                            goto case 1;
+                        }
 
-                    int v = Calculator.Add(a, b);
+                        int v = Calculator.Add(a, b);
 
-                    Console.WriteLine("resultatet er: " + v);
-                    Console.ReadLine();
+                        Console.WriteLine("resultatet er: " + v);
+                        Console.ReadLine();
 
 
-                    break;
+                        break;
+
+                    case 9:
+                        running = false;
+                        break;
+                }
             }
-
 
                         
 
@@ -60,9 +67,18 @@ namespace CalcApp
 
         static int Add()
         {
-            Console.WriteLine("1: addition 2: Subtraktion");
-            return int.Parse(Console.ReadLine());
-           
+            int a = new int();
+            Console.WriteLine("1: addition 2: Subtraktion 9: Exit");
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("prøv igen");
+            }
+
+            return a;
         }
     }
 }
